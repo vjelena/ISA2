f363f1e509e1c42bdd0ddc721b00a6b5e0736d70
Nena <nenavidovic89@gmail.com>
2018-04-12 17:11:57 +0200

    Zapocet rad na izmjeni profila Bioskopa.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,14 @@
+
+
 insert into adresa values (11, '67', 'Novi Sad', 'Dositejeva');
 insert into korisnik values(1, 1, '123456', 'petar@gmail.com' , 'Petar', '123', 'Petrovic', 0, 'obican', 11);
 
-/*insert into adresa values (12, '35', 'Beograd', 'Puskinova');
-insert into korisnik values(2, 1, '095346', 'marko@gmail.com' , 'Marko', '777', 'Markovic', 0, 'admin', 12);*/
+insert into adresa values (12, '35', 'Beograd', 'Puskinova');
+insert into korisnik values(2, 1, '095346', 'marko@gmail.com' , 'Marko', '777', 'Markovic', 0, 'admin', 12);
 
 
 
-
-
-
-
-/*INSERT INTO adresa values (21,'Broj', 'grad', 'ulica');
+INSERT INTO adresa values (21,'Broj', 'grad', 'ulica');
 INSERT INTO clan values (3, 10, 5, 'zlatni');
 
 
@@ -31,8 +29,8 @@ INSERT INTO glumac values (2, 'Walton','Goggins');
 INSERT INTO glumac values (3, 'Olivia','Cooke');
 
 
-INSERT INTO projekcija values (1,'Ready player one', 4.5 , 'opis', 'Steven Spielberg', 'slika', 120, 'drama',1);
-INSERT INTO projekcija values (2,'Tomb raider 3D', 4.5 , 'Lara Kroft je strastvena, samostalna devojka i cerka ekscentricnog pustolova koji je nestao dok je ona jos bila tinejdzerka.', 'Roar Uthaug', 'slika', 120, 'drama',2);
+INSERT INTO projekcija values (1, 120.90, 'Ready player one', 4.5, 'opis', 'Steven Spielberg', 'slika', 120, 'drama',1);
+INSERT INTO projekcija values (2, 150.00, 'Tomb raider 3D', 4.5 , 'Lara Kroft je strastvena, samostalna devojka i cerka ekscentricnog pustolova koji je nestao dok je ona jos bila tinejdzerka.', 'Roar Uthaug', 'slika', 120, 'drama',2);
 
 INSERT INTO sala values(1 ,150,1 ,'konfg',1 ,1 );
 INSERT INTO sala values(2 ,100 ,2 ,'konfg',2 ,2 );
@@ -51,4 +49,4 @@ INSERT INTO projekcija_glumci values (2,2);
 INSERT INTO projekcija_glumci values (3,2);
 
 insert into fan_zona(naziv) values ('jaslkjfa');
-insert into fan_zona(naziv) values ('stgtrgtrdgrt');*/
+insert into fan_zona(naziv) values ('stgtrgtrdgrt');
